39b1d3256a1f9c427c39a0af5dcda8bc085acaec
ebner <simon.ebner@psi.ch>
2016-03-23 15:01:34 +0100

    added option to print messages instead of dumping to file and added option to only dump the first n sub-messages

--- mflow/utils/dump.py
+++ mflow/utils/dump.py
@@ -4,6 +4,7 @@ import signal
 
 counter = 0
 folder = None
+skip_from_message = None
 
 
 def dump(receiver):
@@ -19,6 +20,20 @@ def dump(receiver):
         cnt += 1
 
 
+def dump_screen(receiver):
+    cnt = 0
+    message = receiver.next()
+    cnt += 1
+    if not skip_from_message or cnt < skip_from_message:
+        print(message)
+
+    while receiver.has_more():
+        message = receiver.next()
+        if not skip_from_message or cnt < skip_from_message:
+            print(message)
+        cnt += 1
+
+
 # def stop(*argv):
 #     global receive_more
 #     receive_more = False
@@ -30,18 +45,22 @@ def main():
 
     global counter
     global folder
+    global skip_from_message
 
     parser = argparse.ArgumentParser(description='Stream dump utility')
 
     parser.add_argument('source', type=str, help='Source address - format "tcp://<address>:<port>"')
-    parser.add_argument('folder', type=str, help='Destination folder')
+    parser.add_argument('folder', default=None, nargs='?', type=str, help='Destination folder')
+    parser.add_argument('-s', '--skip', default=None, type=int,
+                        help='Skip sub-messages starting from this number (including number)')
 
     arguments = parser.parse_args()
 
     folder = arguments.folder
     address = arguments.source
+    skip_from_message = arguments.skip
 
-    if not os.path.exists(folder):
+    if folder and not os.path.exists(folder):
         os.makedirs(folder)
 
     stream = mflow.connect(address)
@@ -57,8 +76,13 @@ def main():
 
     signal.signal(signal.SIGINT, stop)
 
+    # Select handler
+    handler = dump
+    if not folder:
+        handler = dump_screen
+
     while receive_more:
-        stream.receive(handler=dump)
+        stream.receive(handler=handler)
         counter += 1
 
 
