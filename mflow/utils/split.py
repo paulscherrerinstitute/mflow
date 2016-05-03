15f74a3cdade30406f5729c364c31d2bf0069275
ebner <simon.ebner@psi.ch>
2016-05-03 11:42:46 +0200

    added a way to pass a more complex configuration to the splitter

--- mflow/utils/split.py
+++ mflow/utils/split.py
@@ -23,24 +23,35 @@ class Splitter:
 def main():
 
     import argparse
+    import sys
 
     parser = argparse.ArgumentParser(description='Stream dump utility')
 
-    parser.add_argument('source', type=str, help='Source address - format "tcp://<address>:<port>"')
-    parser.add_argument('streams', type=str, nargs='+', help='Streams to generate - "tcp://<address>:<port>"')
+    parser.add_argument('-c', '--config', help='Configuration file')
+
+    parser.add_argument('source', type=str, nargs='?', help='Source address - format "tcp://<address>:<port>"')
+    parser.add_argument('streams', type=str, nargs='*', help='Streams to generate - "tcp://<address>:<port>"')
 
     arguments = parser.parse_args()
 
-    streams_to_generate = arguments.streams
-    address = arguments.source
+    if arguments.config:
+        print('config')
+        (input_stream, splitter) = load_configuration(arguments.config)
+    elif arguments.source and arguments.streams:
+        streams_to_generate = arguments.streams
+        address = arguments.source
 
-    output_streams = []
-    for new_stream in streams_to_generate:
-        output_streams.append(mflow.connect(new_stream, conn_type=mflow.BIND, mode=mflow.PUSH))
+        output_streams = []
+        for new_stream in streams_to_generate:
+            output_streams.append(mflow.connect(new_stream, conn_type=mflow.BIND, mode=mflow.PUSH))
 
-    splitter = Splitter(output_streams)
+        splitter = Splitter(output_streams)
+        input_stream = mflow.connect(address)
+    else:
+        parser.print_help()
+        sys.exit(-1)
 
-    input_stream = mflow.connect(address)
+    # Info: By here splitter and input_stream needs to be specified
 
     # Signal handling
     global receive_more
@@ -55,6 +66,70 @@ def main():
 
     while receive_more:
         input_stream.receive(handler=splitter.receive)
+
+
+def load_configuration(filename):
+    """
+    Read in a configuration file like this:
+    {
+        "source": {
+            "address": "tcp://localhost:7777",
+            "mode": "PULL"
+        },
+        "streams": [
+            {
+                "address": "tcp://*:8888",
+                "mode": "PUSH"
+            }
+        ]
+    }
+    """
+
+    import json
+    import re
+
+    # Load configuration file
+    with open(filename) as file_handle:
+        configuration = json.load(file_handle)
+
+    # Construct stream source
+    address = configuration['source']['address']
+    if re.match('tcp://\\*:.*', address):
+        connection_type = mflow.BIND
+    else:
+        connection_type = mflow.CONNECT
+    mode = mflow.PULL
+    if 'mode' in configuration['source']:
+        if configuration['source']['mode'].lower() == 'pull':
+            mode = mflow.PULL
+        elif configuration['source']['mode'].lower() == 'sub':
+            mode = mflow.SUB
+        else:
+            raise Exception('Unsupported mode [%s] for source [%s]' % (configuration['source']['mode'], configuration['source']))
+    print(connection_type)
+    input_stream = mflow.connect(address, mode=mode, conn_type=connection_type)
+
+    # Construct output streams
+    output_streams = []
+    for stream in configuration['streams']:
+        address = stream['address']
+        if re.match('tcp://\\*:.*', address):
+            connection_type = mflow.BIND
+        else:
+            connection_type = mflow.CONNECT
+
+        mode = mflow.PUSH
+        if 'mode' in stream:
+            if stream['mode'].lower() == 'push':
+                mode = mflow.PUSH
+            elif stream['mode'].lower() == 'pub':
+                mode = mflow.PUB
+            else:
+                raise Exception('Unsupported mode [%s] for stream [%s]' % (stream['mode'], stream))
+
+        output_streams.append(mflow.connect(address, conn_type=connection_type, mode=mode))
+
+    return input_stream, Splitter(output_streams)
 
 
 if __name__ == '__main__':
