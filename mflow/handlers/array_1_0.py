cf2205745ced84e42cd9ddf33cbaca4ddeea4fe6
Andrej Babic <andrej.babic@psi.ch>
2017-03-06 15:20:13 +0100

    Array 1.0 handler send bytes fix
    Increase mflow version

--- mflow/handlers/array_1_0.py
+++ mflow/handlers/array_1_0.py
@@ -10,7 +10,7 @@ class Handler:
 
         header = receiver.next(as_json=True)
 
-        return_value = {}
+        return_value = None
         data = []
 
         # header contains: "htype", "shape", "type", "frame", "endianess", "source", "encoding", "tags"
@@ -32,7 +32,19 @@ class Handler:
     @staticmethod
     def send(message, send, block=True):
         send(json.dumps(message["header"]).encode(), send_more=True, block=True)
-        send(message["data"][0].tobytes(), block=block)
+
+        # Get the number of data segments.
+        number_of_segments = len(message["data"])
+
+        # Forward all data segments available.
+        for segment_index in range(number_of_segments):
+            data_segment = message["data"][segment_index]
+            more_blocks_to_send = segment_index + 1 != number_of_segments
+            # Get the bytes for the data if the data is not already in bytes.
+            if not isinstance(data_segment, bytes):
+                data_segment = data_segment.tobytes()
+
+            send(data_segment, block=block, send_more=more_blocks_to_send)
 
 
 def get_image(raw_data, dtype, shape):
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='mflow',
-      version='0.0.16',
+      version='0.0.17',
       description='Utility library for ZMQ data streaming',
       author='Paul Scherrer Institute',
       license='GNU GPLv3',
