e44b7fd155dcc3310fa7f637b9f66b349762d881
Andrej Babic <andrej.babic@psi.ch>
2017-03-21 15:36:21 +0100

    Improve mflow stats utility
    Because we want to measure the mflow performance consistently,
    we need a standardized way to measure throughput.
    The new StreamStatisticsPrinter should be used for all measurements.

--- mflow/tools.py
+++ mflow/tools.py
@@ -1,3 +1,6 @@
+import time
+
+
 class RoundRobinStrategy:
 
     def __init__(self):
@@ -44,3 +47,112 @@ class Merge:
         for stream in self.streams:
             stream.disconnect()
 
+
+class StreamStatisticsPrinter(object):
+    """
+    Record and display the user the stream message and bytes throughput.
+    """
+
+    # Bytes to mega bytes conversion factor.
+    MB_FACTOR = 1 / (10 ** 6)
+
+    def __init__(self, sampling_interval=0.2):
+        """
+        Initiate the stream statistics printer.
+        :param sampling_interval: Minimum sampling interval.
+        """
+        self.sampling_interval = sampling_interval
+
+        # Collect the initial time in case you need to print the summary.
+        self.initial_time = time.time()
+
+        # Keep track of the last statistics printed to the user.
+        self.last_printed_statistics = {"total_bytes_received": 0,
+                                        "messages_received": 0,
+                                        "time": self.initial_time}
+
+        # Keep track of the last statistics received.
+        self.last_received_statistics = {"total_bytes_received": 0,
+                                         "messages_received": 0,
+                                         "time": self.initial_time}
+
+    def process_statistics(self, message_statistics):
+        """
+        Should be called at every message received.
+        :param message_statistics: Statistics of the received message.
+        """
+        current_time = time.time()
+
+        # Save received statistics.
+        self.last_received_statistics["total_bytes_received"] = message_statistics.total_bytes_received
+        self.last_received_statistics["messages_received"] = message_statistics.messages_received
+        self.last_received_statistics["time"] = current_time
+
+        delta_time = current_time - self.last_printed_statistics["time"]
+        # If the delta time is greater than the sampling rate, print the statistics.
+        if delta_time > self.sampling_interval:
+            self.print_statistics()
+
+    def print_statistics(self):
+        """
+        Calculate the data rate and print the statistics to the standard output.
+        """
+        delta_time = self.last_received_statistics["time"] - self.last_printed_statistics["time"]
+
+        # bytes/second in last interval.
+        data_rate = (self.last_received_statistics["total_bytes_received"] -
+                     self.last_printed_statistics["total_bytes_received"]) / delta_time
+
+        # messages/second in last interval.
+        message_rate = (self.last_received_statistics["messages_received"] -
+                        self.last_printed_statistics["messages_received"]) / delta_time
+
+        output = "Data rate: {data_rate: >10.3f} MB/s    Message rate: {message_rate: >10.3f} Hz" \
+            .format(data_rate=data_rate * self.MB_FACTOR, message_rate=message_rate)
+
+        print(output)
+
+        # Update last printed statistics.
+        self.last_printed_statistics.update(self.last_received_statistics)
+
+    def print_summary(self):
+        """
+        Print statistics summary to standard output.
+        """
+        # Total elapsed time.
+        delta_time = self.last_received_statistics["time"] - self.initial_time
+        # Print summary if any messages were received.
+        if delta_time > 0:
+            print("_" * 60)
+
+            data_rate = self.last_received_statistics["total_bytes_received"] / delta_time
+            message_rate = self.last_received_statistics["messages_received"] / delta_time
+
+            print("Total elapsed time:   {: >10.3f} s".format(delta_time))
+            print("Average message size: {: >10.3f} MB".format(
+                self.last_received_statistics["total_bytes_received"] /
+                self.last_received_statistics["messages_received"] * self.MB_FACTOR))
+            print("Total bytes received: {: >10.3f} MB".format(
+                self.last_received_statistics["total_bytes_received"] * self.MB_FACTOR))
+            print("Average data rate:    {: >10.3f} MB/s".format(data_rate * self.MB_FACTOR))
+            print("Messages received:    {: >10d} messages".format(self.last_received_statistics["messages_received"]))
+            print("Average message rate: {: >10.3f} Hz".format(message_rate))
+        # No messages were received.
+        else:
+            print("No messages received.")
+
+        print("_" * 60)
+
+    def close(self, print_summary=True):
+        """
+        You need to close the statistics in order to flush any non printed statistics.
+        :param print_summary: True if you want the summary to be printed.
+        """
+        # Note: The sampling interval, when flushing, is not honored.
+        delta_time = self.last_received_statistics["time"] - self.last_printed_statistics["time"]
+        # If the last received is not the same as the last printed statistics, print it.
+        if delta_time > 0:
+            self.print_statistics()
+
+        if print_summary:
+            self.print_summary()
--- mflow/utils/stats.py
+++ mflow/utils/stats.py
@@ -1,99 +1,51 @@
 import mflow
-import signal
+import argparse
 
-counter = 0
-folder = None
-
-messages_received = 0
-total_bytes_received = 0
-message_rate = 0
-receive_rate = 0
-
-
-def dump(receiver):
-    receiver.next()
-    while receiver.has_more():
-        receiver.next()
-
-    return "data"
+from mflow.tools import StreamStatisticsPrinter
 
 
 def main():
-
-    import time
-    import argparse
     parser = argparse.ArgumentParser(description='Stream statistic utility')
-
     parser.add_argument('source', type=str, help='Source address - format "tcp://<address>:<port>"')
-    parser.add_argument('-m', '--mode', default='pull', type=str,
+    parser.add_argument('-m', '--mode', default='pull', choices=['pull', 'sub'], type=str,
                         help='Communication mode - either pull (default) or sub')
-
+    parser.add_argument('-i', "--sampling_interval", type=float, default=0.5,
+                        help="Interval in seconds at which to sample the stream.\n"
+                             "If zero, every packet will be sampled.")
     arguments = parser.parse_args()
 
     address = arguments.source
     mode = mflow.SUB if arguments.mode == 'sub' else mflow.PULL
+    stream = mflow.connect(address, mode=mode, receive_timeout=1000)
+    statistics_printer = StreamStatisticsPrinter(sampling_interval=arguments.sampling_interval)
 
-    stream = mflow.connect(address, mode=mode)
+    def dump(receiver):
+        """
+        Just read the stream.
+        :param receiver: Function to use as a receiver.
+        :return: 1 if the reception was successful, None if it timed out.
+        """
+        data = receiver.next()
+        while receiver.has_more():
+            # If any of the message parts time outed (the only way a message part can be None)
+            if receiver.next() is None:
+                return None
+        # Return 1 only for valid data.
+        return 1 if data else None
 
-    global messages_received
-    global total_bytes_received
-    global message_rate
-    global receive_rate
+    print("mflow stats started. Sampling interval is %.2f seconds." % arguments.sampling_interval)
+    print("_" * 60)
 
-    # Signal handling
-    global more
-    more = True
+    try:
+        while True:
+            message = stream.receive(handler=dump)
+            if message is not None:
+                statistics_printer.process_statistics(message.statistics)
+    except KeyboardInterrupt:
+        stream.disconnect()
+        # Flush and Print summary.
+        statistics_printer.close()
 
-    def stop(*arguments):
-        global more
-        more = False
-        print_statistics()
-        signal.siginterrupt()
-
-    signal.signal(signal.SIGINT, stop)
-
-    previous_time = time.time()
-    previous_total_bytes_received = 0
-    previous_messages_received = 0
-
-    while more:
-        message = stream.receive(handler=dump)
-
-        now = time.time()
-        delta_time = now - previous_time
-
-        # Print every second
-        # TODO Need to be done differently as at the end of a stream the last stats do not show up
-        # Use threading.Timer(1, foo).start()
-        # (http://stackoverflow.com/questions/8600161/executing-periodic-actions-in-python)
-        # As printing out every time a message is received will slow down the receive process
-        # if delta_time > 0.1:
-        if 1:
-
-            total_bytes_received = message.statistics.total_bytes_received
-            messages_received = message.statistics.messages_received
-
-            receive_rate = (total_bytes_received - previous_total_bytes_received) / delta_time
-            message_rate = (messages_received - previous_messages_received) / delta_time
-
-            previous_total_bytes_received = total_bytes_received
-            previous_messages_received = messages_received
-
-            if delta_time > 0.1:
-                previous_time = now
-                print_statistics()
-
-
-def print_statistics():
-    print(chr(27) + "[2J")
-    print("_"*60)
-    print('Messages received: {}'.format(messages_received))
-    print('Total bytes received: {} Mb'.format(total_bytes_received/1024.0/1024.0))
-
-    print("Message rate: {} Hz".format(message_rate))
-    print("Receive rate: {} Mbps".format(receive_rate/1024/1024*8))
-    print("_"*60)
-    print('')
 
 if __name__ == '__main__':
     main()
