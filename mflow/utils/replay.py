c843e3777693d99e9bd92a718241ecd3146bf531
Andrej Babic <andrej.babic@psi.ch>
2017-11-20 10:42:29 +0100

    Extract the main function of m_reply
    The same function can be now used inside unit tests to
    execute some dumped data.

--- mflow/utils/replay.py
+++ mflow/utils/replay.py
@@ -1,9 +1,31 @@
 import os
 import mflow
-import zmq
 from os import listdir
 from os.path import isfile, join
 import argparse
+
+
+def reply_folder(bind_address, folder, mode):
+
+    if not os.path.exists(folder):
+        raise ValueError("Specified folder '%s' does not exist.")
+
+    stream = mflow.connect(bind_address, conn_type="bind", mode=mode)
+
+    files = sorted(listdir(folder))
+
+    for index, raw_file in enumerate(files):
+        filename = join(folder, raw_file)
+        if not (raw_file.endswith('.raw') and isfile(filename)):
+            continue
+
+        with open(filename, mode='rb') as file_handle:
+            send_more = False
+            if index + 1 < len(files):  # Ensure that we don't run out of bounds
+                send_more = raw_file.split('_')[0] == files[index + 1].split('_')[0]
+
+            print('Sending %s [%s]' % (raw_file, send_more))
+            stream.send(file_handle.read(), send_more=send_more)
 
 
 def main():
@@ -22,25 +44,7 @@ def main():
     address = arguments.address
     mode = mflow.PUB if arguments.mode == 'pub' else mflow.PUSH
 
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    stream = mflow.connect(address, conn_type="bind", mode=mode)
-
-    files = sorted(listdir(folder))
-
-    for index, raw_file in enumerate(files):
-        filename = join(folder, raw_file)
-        if not (raw_file.endswith('.raw') and isfile(filename)):
-            continue
-
-        with open(filename, mode='rb') as file_handle:
-            send_more = False
-            if index+1 < len(files):  # Ensure that we don't run out of bounds
-                send_more = raw_file.split('_')[0] == files[index+1].split('_')[0]
-
-            print('Sending %s [%s]' % (raw_file, send_more))
-            stream.send(file_handle.read(), send_more=send_more)
+    reply_folder(address, folder, mode)
 
 
 if __name__ == '__main__':
