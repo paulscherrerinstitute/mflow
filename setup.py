fe9db2e403aa0cbf6a28a81149033d8877cef8cf
Andrej Babic <andrej.babic@psi.ch>
2020-03-31 12:26:07 +0200

    New library version
    - Zero copy recv/send
    - Noarch build
    - Fix of deprecated functions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='mflow',
-      version='0.1.0',
+      version='0.2.0',
       description='Utility library for ZMQ data streaming',
       author='Paul Scherrer Institute',
       license='GNU GPLv3',
