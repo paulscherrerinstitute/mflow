00406a341093f49d9f75aff4817dfa404af02153
Sven Augustin <sven.augustin@psi.ch>
2025-11-20 09:52:21 +0100

    this is the oddest yet ...

--- mflow/handlers/__init__.py
+++ mflow/handlers/__init__.py
@@ -2,6 +2,9 @@
 import glob
 import os
 
+
+# automatically populates __all__ with the handler modules
+# this is not needed as it is the default for the current file/folder structure anyway
 __all__ = []
 htypes = {}
 for f in glob.glob(os.path.dirname(__file__) + "/*.py"):
@@ -9,6 +12,8 @@ for f in glob.glob(os.path.dirname(__file__) + "/*.py"):
         __all__.append(os.path.basename(f)[:-3])
 
 
+# converts a human-readable xyz-1.0 to a module xyz_1_0.py and imports it
+# this is not needed (and not used) since the two dicts in mflow.py (receive_handlers and send_handlers) handle this explicitly
 def load(htype):
     # Todo: Add some more logic to use an more general handler if possible
     # i.e. htype-1.2 can be handled with an htype_1.py handler
