1a16026c82e3aee0f743b035ef9af40ff3770a24
Sven Augustin <sven.augustin@psi.ch>
2025-11-20 12:01:10 +0100

    added DeprecationWarning for mflow.tools

--- mflow/__init__.py
+++ mflow/__init__.py
@@ -1,8 +1,27 @@
 
 from .mflow import *
 
+
 #TODO: deprecate the tools name in favor of utils for consistency with all our other projects
-#TODO: can this throw a DeprecationWarning?
-from . import utils as tools
+class _DeprecationWrapper:
+
+    def __init__(self, deprecated, message):
+        self.deprecated = deprecated
+        self.message = message
+
+    def __getattr__(self, name):
+        import warnings
+        warnings.warn(self.message, category=DeprecationWarning, stacklevel=2)
+        return getattr(self.deprecated, name)
+
+    def __dir__(self):
+        return dir(self.deprecated)
+
+    def __repr__(self):
+        return repr(self.deprecated)
+
+
+from . import utils
+tools = _DeprecationWrapper(utils, "the `mflow.tools` alias is deprecated; use `mflow.utils` instead")
 
 
