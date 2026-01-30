0840323f089de333e56675e430cfc6a1a4ee093c
gobbo_a <alexandre.gobbo@psi.ch>
2026-01-30 13:47:18 +0100

    Support building pip package

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import find_packages, setup
 
+PACKAGE_PREFIX = "psi-"
+PACKAGE_NAME = "mflow"
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="mflow",
+    name=PACKAGE_PREFIX + PACKAGE_NAME,
     version="1.0.0",
-    url="https://github.com/paulscherrerinstitute/mflow",
+    url="https://github.com/paulscherrerinstitute/" + PACKAGE_NAME,
     description="Utility library for ZMQ data streaming",
     author="Paul Scherrer Institute",
     license="GNU GPLv3",
-    requires=["pyzmq"],
-    packages=find_packages()
+    install_requires=["numpy", "pyzmq"],
+    packages=find_packages(),
+    long_description=read('Readme.md'),
+    long_description_content_type="text/markdown",
+
 )
 
 
